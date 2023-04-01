70994d75591453eabb30dc187f59aaa08852d107
Ryan Horn <rj.horn@gmail.com>
2023-04-01 14:10:19 -0600

    routes working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Route, Routes } from 'react-router-dom'
+
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Account from "./components/Account";
 
 function App() {
   return (
     <div>
-      <p>hi</p>
+      <h1>Initial Auth</h1>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
     </div>
   );
 }
