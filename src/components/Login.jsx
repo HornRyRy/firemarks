2afbcb4cecc27290cc2ed5293b14177d68b1e18d
Ryan Horn <rj.horn@gmail.com>
2023-04-01 14:45:33 -0600

    Login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+    <h1>Login Page</h1>
+    <p>Don't have an account? <Link to='/signup'>Sign up</Link></p>
+    <form>
+      <h2>Log in Here!</h2>
+      <div>
+      <label>Email</label>
+      <input
+      placeholder="Email (for username)"
+      type="text"
+      name="email"
+      value={""} />
+      </div>
+      <div>
+          <label>Password</label>
+          <input
+          placeholder="Password"
+          type="password"
+          name="password"
+          value={""} />
+      </div>
+      <div>
+          <input
+          type="submit"
+          value="Log In" />
+      </div>
+    </form>
+  </div>
   )
 }
 
