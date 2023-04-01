92af007f55acbbaccee833aa375b45c90695a72b
Ryan Horn <rj.horn@gmail.com>
2023-04-01 16:39:09 -0600

    logout works

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
 
 const Account = () => {
 
   const {user, logout} = UserAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = () =>{
+     logout()
+     navigate('/')
+     console.log('user has been logged out')
+  }
 
   return (
     <div>
         <h1>Account</h1>
         <p>User Email {user && user.email}</p>
-        <button>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
