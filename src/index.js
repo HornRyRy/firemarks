74d38fe7cf83db6a02d5659515963040334f2c82
Ryan Horn <rj.horn@gmail.com>
2023-04-04 12:07:49 -0600

    memory router working as router in extension

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <MemoryRouter>
     <App />
-  </BrowserRouter>
+  </MemoryRouter>
 );
 
 
